f441f8d9588a2e7ebbf2eddef117e7ee39ba9d2a
constracti <constracti@gmail.com>
2019-02-14 00:18:44 +0200

    check NIfTI data instead of fdata

--- nifticmp.py
+++ nifticmp.py
@@ -1,21 +1,24 @@
 import argparse
 import os
-import nibabel
+import re
+
 import numpy
+import nibabel
 
 def _path2obj(path):
 	assert os.path.isfile(path)
+	assert re.search("\.nii(?:\.gz)$", path, flags=re.I)
 	return nibabel.load(path)
 
 def cmp(nifti1, nifti2):
 	if type(nifti1) is str:
 		nifti1 = _path2obj(nifti1)
 	if type(nifti1) is not numpy.ndarray:
-		nifti1 = nifti1.get_fdata()
+		nifti1 = nifti1.get_data()
 	if type(nifti2) is str:
 		nifti2 = _path2obj(nifti2)
 	if type(nifti2) is not numpy.ndarray:
-		nifti2 = nifti2.get_fdata()
+		nifti2 = nifti2.get_data()
 	ret = numpy.all(nifti1 == nifti2)
 	if __name__ == "__main__":
 		exit(not ret)
