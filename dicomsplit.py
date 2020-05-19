9495557650674361328568dfff2af31b8e67af22
constracti <constracti@gmail.com>
2020-05-19 12:28:33 +0300

    split according to custom tags

--- dicomsplit.py
+++ dicomsplit.py
@@ -8,67 +8,75 @@ import pydicom
 
 import dicomtools
 
-def split(path, move=False, force=False, single=False, verbose=False):
+def _get_tag(dicom, tags):
+	if type(dicom) is str:
+		dicom = pydicom.dcmread(dicom, specific_tags=tags)
+	return "-".join(str(dicom[tag].value) for tag in tags)
+
+def split(path, tags=[], move=False, force=False, single=False, verbose=False):
 	assert os.path.isdir(path), "path {} is not a directory".format(path)
 	# find DICOM files
 	files = []
-	series = []
+	dcmsets = []
 	for filename in os.listdir(path):
 		filepath = os.path.join(path, filename)
 		if not os.path.isfile(filepath):
 			continue
 		try:
-			aseries = dicomtools.get_series(filepath)
+			if tags:
+				dcmset = _get_tag(filepath, tags)
+			else:
+				dcmset = dicomtools.get_series(filepath)
 		except pydicom.errors.InvalidDicomError:
 			continue
-		files.append((filename, filepath, aseries))
-		if aseries not in series:
-			series.append(aseries)
+		files.append((filename, filepath, dcmset))
+		if dcmset not in dcmsets:
+			dcmsets.append(dcmset)
 	if verbose:
-		print("found {} DICOM files with {} different series".format(len(files), len(series)))
-	if len(series) > 1 or len(series) == 1 and single:
-
+		print("found {} DICOM files with {} different series".format(len(files), len(dcmsets)))
+	if len(dcmsets) > 1 or len(dcmsets) == 1 and single:
 		# check subdirectories
 		if not force:
-			for aseries in series:
-				dirpath = os.path.join(path, aseries)
-				assert not os.path.isfile(dirpath), "file {} exists".format(aseries)
-				assert not os.path.isdir(dirpath), "directory {} exists".format(aseries)
+			for dcmset in dcmsets:
+				dirpath = os.path.join(path, dcmset)
+				assert not os.path.isfile(dirpath), "file {} exists".format(dcmset)
+				assert not os.path.isdir(dirpath), "directory {} exists".format(dcmset)
 		# create subdirectories
-		for aseries in series:
-			dirpath = os.path.join(path, aseries)
+		for dcmset in dcmsets:
+			dirpath = os.path.join(path, dcmset)
 			if os.path.isfile(dirpath):
 				if verbose:
-					print("removing file {}".format(aseries))
+					print("removing file {}".format(dcmset))
 				os.remove(dirpath)
 			elif os.path.isdir(dirpath):
 				if verbose:
-					print("removing directory {}".format(aseries))
+					print("removing directory {}".format(dcmset))
 				shutil.rmtree(dirpath)
 			if verbose:
-				print("creating directory {}".format(aseries))
+				print("creating directory {}".format(dcmset))
 			os.mkdir(dirpath)
 		# place DICOM files to subdirectories 
-		for i, (filename, filepath, aseries) in enumerate(files):
-			newpath = os.path.join(path, aseries, filename)
+		for i, (filename, filepath, dcmset) in enumerate(files):
+			newpath = os.path.join(path, dcmset, filename)
 			if move:
 				if verbose:
-					print("[{}/{}] moving DICOM file {} to directory {}".format(i + 1, len(files), filename, aseries))
+					print("[{}/{}] moving DICOM file {} to directory {}".format(i + 1, len(files), filename, dcmset))
 				shutil.move(filepath, newpath)
 			else:
 				if verbose:
-					print("[{}/{}] copying DICOM file {} to directory {}".format(i + 1, len(files), filename, aseries))
+					print("[{}/{}] copying DICOM file {} to directory {}".format(i + 1, len(files), filename, dcmset))
 				shutil.copy2(filepath, newpath)
 	if verbose:
 		print("dicomsplit complete")
-	return series
+	return dcmsets
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Place each DICOM file in a subdirectory according to Protocol Name and Series Number.")
 	parser.add_argument("path", help="directory of mixed DICOM files", metavar="PATH")
 	parser.add_argument("-m", "--move", action="store_true", help="move files instead of copying")
+	parser.add_argument("-t", "--tag", action="append", help="split DICOM files according to a custom tag", dest="tags")
 	parser.add_argument("-f", "--force", action="store_true", help="overwrite existing subdirectories")
 	parser.add_argument("-s", "--single", action="store_true", help="run even if all DICOM files belong to the same series")
 	parser.add_argument("-v", "--verbose", action="store_true", help="print actions")
 	args = parser.parse_args()
-	split(args.path, move=args.move, force=args.force, single=args.single, verbose=args.verbose)
+	split(args.path, tags=args.tags, move=args.move, force=args.force, single=args.single, verbose=args.verbose)
